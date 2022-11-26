bce6f9ec24f56d1cddced80f4b16b5cdcfa2945a
Tsubasa-Oba <tk2830717@yahoo.co.jp>
2022-11-26 16:34:15 +0900

    bladeファイル一部修正　@includeでヘッダー・フッダーを作成

--- resources/views/nekocafe/form.blade.php
+++ resources/views/nekocafe/form.blade.php
@@ -7,28 +7,7 @@
     <title>猫カフェ にゃんこのおなか お問い合わせ画面</title>
     @vite(['resources/css/destyle.css', 'resources/css/nekocafeTop.css', 'resources/css/nekocafeProf.css',  'resources/css/nekocafeForm.css', 'resources/js/nekocafeMovie.js'])
 </head>
-<header class="header header_floating">
-            <div class="l_container">
-                <div class="l_logo_nav">
-                    <div class="l_left_logo">
-                        <a href="{{  $indexViewData['TOP'] }}"><img src="{{ asset('/storage/site/SiteLogo.png') }}" alt="にゃんこのせなかロゴ"></a>  
-                    </div>      
-                    <nav class="l_nav_right_container">
-                        <ul class="p_nav l_nav_list">
-                            <li><a href="{{  $indexViewData['CONCEPT'] }}" class="c_text_nav">CONCEPT</a></li>
-                            <li><a href="{{  $indexViewData['CATS'] }}" class="c_text_nav">CATS</a></li>
-                            <li><a href="{{  $indexViewData['ACCESS'] }}" class="c_text_nav">ACCESS</a></li>
-                            <li><a href="{{  $indexViewData['CONTACT'] }}" class="c_text_nav">CONTACT</a></li>
-                        </ul>
-                        <ul class="p_nav l_nav_btn">
-                            <li><a href="https://twitter.com/nyanko_no_onaka" class="c_btn_nav"><img src="{{ asset('/storage/site/TwitterIcon.png') }}" alt="にゃんこのせなかTwitter"></a></li>
-                            <li><a href="https://www.instagram.com/nyanko_no_onaka" class="c_btn_nav"><img src="{{ asset('/storage/site/InstagramIcon.png') }}" alt="にゃんこのせなかInstagram"></a></li>
-                            <li><a href="" class="c_btn_nav"><img src="{{ asset('/storage/site/YoutubeIcon.png') }}" alt="にゃんこのせなかYoutube"></a></li>
-                        </ul>           
-                    </nav>
-                </div>
-            </div>
-</header>
+@include('nekocafe.common.head')
 <body class="l_body">
     <div class="l_contents">
         <div class="l_container">
@@ -116,15 +95,5 @@
 
     </div>
 </body>
-<footer class="footer">
-    <div class="l_contents">
-        <div class="l_container">
-            <div class="l_footer f_footer_text">
-                <a href="{{  $indexViewData['TOP'] }}"><img src="{{ asset('/storage/site/SiteLogo.png') }}" alt="にゃんこのせなかロゴ"></a>
-                <div>住所：埼玉県所沢市○○○○</div>
-                <div>© 2022 Tsubasa_Oba</div>     
-            </div>                          
-        </div>
-    </div>
-</footer>
+@include('nekocafe.common.foot')
 </html>
--- resources/views/nekocafe/index.blade.php
+++ resources/views/nekocafe/index.blade.php
@@ -118,15 +118,5 @@
         </div>
     </div>
 </body>
-<footer class="footer">
-    <div class="l_contents">
-        <div class="l_container">
-            <div class="l_footer f_footer_text">
-                <a href=""><img src="{{ asset('/storage/site/SiteLogo.png') }}" alt="にゃんこのせなかロゴ"></a>
-                <div>住所：埼玉県所沢市○○○○</div>
-                <div>© 2022 Tsubasa_Oba</div>     
-            </div>                          
-        </div>
-    </div>
-</footer>
+@include('nekocafe.common.foot')
 </html>
