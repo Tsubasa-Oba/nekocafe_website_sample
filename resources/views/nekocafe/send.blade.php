c2b1e3d3c89638ff485c15b72a7c94b71ca03729
Tsubasa-Oba <tk2830717@yahoo.co.jp>
2022-11-29 20:42:57 +0900

    fix:faviconの変更

--- resources/views/nekocafe/send.blade.php
+++ resources/views/nekocafe/send.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon"  href="{{ asset('/favicon.ico') }}">
     <title>猫カフェ にゃんこのおなか　送信完了</title>
     <link href="{{ asset('/css/destyle.css' )}}" rel="stylesheet">
     <link href="{{ asset('/css/nekocafeTop.css' )}}" rel="stylesheet">
